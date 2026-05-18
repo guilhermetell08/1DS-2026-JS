4b26303e0a33531e9e670ecb9435f406a89b8474
satanás <guilhermemaidl5310@gmail.com>
2026-05-18 09:20:03 -0300

    Add files via upload

---
+++ aula03.js
@@ -0,0 +1,20 @@
+let nome = "Tell"
+
+if (nome == "Tell"){
+    console.log("Você digitou o nome correto!")
+}else{
+    console.log("Você digitou outra coisa...")
+}
+
+let idade = "18";
+
+if (idade == 18) {
+    console.log("Você é praticamente adulto")
+}else if( idade < 18){
+    console.log("Você ainda é de menor...")
+}else{
+    console.log("Você é adulto")
+}
+
+//FAÇA UMA VERICAÇÃO COM IF E ELSE
+//DE UMA PESSOA SE ELA PODE OU NÃO VOTAR.
